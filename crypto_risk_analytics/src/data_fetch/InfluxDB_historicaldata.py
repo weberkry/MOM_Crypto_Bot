66cab51f8886fc95bf3f51e9f43fbe6494d7bd01
weberkry <christiane.weber@web.de>
2025-08-31 17:03:45 +0200

    historical datasets as python file

---
+++ crypto_risk_analytics/src/data_fetch/InfluxDB_historicaldata.py
@@ -0,0 +1,15 @@
+import sys, os
+
+# go up one level from the notebook folder
+sys.path.append(os.path.abspath("../analysis"))
+sys.path.append(os.path.abspath("../config"))
+
+import Mandelbrot #defined/customized functions from Mandelbrot.py
+import influxDB_utils as influx
+from influxdb_client import Point
+from datetime import datetime, timezone
+
+for c in ["ETH"]:
+    MIN, HOUR, DAY, WEEK, MONTH = Mandelbrot.historical_datasets(crypto=c, curr="EUR")
+    for INTERVAL in [MIN,HOUR,DAY,WEEK,MONTH]:
+        influx.write_dataframe(INTERVAL)
