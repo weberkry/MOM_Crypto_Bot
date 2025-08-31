9bdb8e22eb4a3f9472c8062c14f4a7ca93c331e0
weberkry <christiane.weber@web.de>
2025-08-31 20:19:21 +0200

    split in chunks for large DF

--- crypto_risk_analytics/src/data_fetch/InfluxDB_historicaldata.py
+++ crypto_risk_analytics/src/data_fetch/InfluxDB_historicaldata.py
@@ -1,15 +1,25 @@
 import sys, os
 
 # go up one level from the notebook folder
-sys.path.append(os.path.abspath("../analysis"))
-sys.path.append(os.path.abspath("../config"))
+sys.path.append(os.path.abspath("/home/weberkry/git/MOM_Crypto_Bot/crypto_risk_analytics/src/analysis"))
+sys.path.append(os.path.abspath("/home/weberkry/git/MOM_Crypto_Bot/crypto_risk_analytics/src/config"))
 
 import Mandelbrot #defined/customized functions from Mandelbrot.py
 import influxDB_utils as influx
 from influxdb_client import Point
 from datetime import datetime, timezone
 
-for c in ["BTC","ETH"]:
+for c in ["ETH"]:
     MIN, HOUR, DAY, WEEK, MONTH = Mandelbrot.historical_datasets(crypto=c, curr="EUR")
-    for INTERVAL in [MIN,HOUR,DAY,WEEK,MONTH]:
+    for INTERVAL in [WEEK,MONTH]:
         influx.write_dataframe(INTERVAL)
+    for INTERVAL in [DAY,HOUR,MIN]:
+        chunk_size = 1000
+        total_rows = len(INTERVAL)
+
+        for start in range(0, total_rows, chunk_size):
+            chunk = INTERVAL.iloc[start:start + chunk_size]
+            influx.write_dataframe(chunk)
+            print(f"Wrote rows {start}-{start+len(chunk)-1} ")
+	
+	
